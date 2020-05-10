5b46bc7668a529801c342dda02bcc03783b3d581
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-10 23:00:31 +0200

    started adjusting the Card template to a separated component

--- src/JokeCard.js
+++ src/JokeCard.js
@@ -1,5 +1,34 @@
 import React from 'react'
 
 export default function JokeCard() {
-  return <div></div>
+  return (
+    <Card key={joke.id} className={classes.card}>
+      <CardContent className={classes.cardContent}>
+        {joke.categories.length > 0 ? (
+          joke.categories.map((cat) => (
+            <Category label={cat} variant='outlined' />
+          ))
+        ) : (
+          <Category label='regular' variant='outlined' />
+        )}
+        <Typography>{joke.joke}</Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={() => likeJoke(joke.id)}
+        >
+          Like
+        </Button>
+        <Button
+          variant='contained'
+          color='default'
+          onClick={() => unlikeJoke(joke.id)}
+        >
+          Unlike
+        </Button>
+      </CardActions>
+    </Card>
+  )
 }
