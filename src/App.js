7ce684d5f4624b49adf72d137bafa0859a3705d0
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-11 22:20:32 +0200

    implemented JokeCard component for likedJokes

--- src/App.js
+++ src/App.js
@@ -88,39 +88,7 @@ function App() {
           </div>
           <div role='tabpanel' hidden={currentTab !== 1}>
             {likedJokes.map((joke) => (
-              <Card key={joke.id} className={classes.card}>
-                <CardContent className={classes.cardContent}>
-                  {joke.categories.length > 0 ? (
-                    joke.categories.map((cat) => (
-                      <Category label={cat} variant='outlined' />
-                    ))
-                  ) : (
-                    <Category label='regular' variant='outlined' />
-                  )}
-                  <Typography>{joke.joke}</Typography>
-                </CardContent>
-                <CardActions className={classes.cardActions}>
-                  <Button
-                    variant='contained'
-                    color='primary'
-                    onClick={() => likeJoke(joke.id)}
-                  >
-                    Like
-                  </Button>
-                  <Button
-                    variant='contained'
-                    color='default'
-                    onClick={() => unlikeJoke(joke.id)}
-                  >
-                    Unlike
-                  </Button>
-                </CardActions>
-              </Card>
-            ))}
-          </div>
-          <div role='tabpanel' hidden={currentTab !== 1}>
-            {likedJokes.map((joke) => (
-
+              <JokeCard key={joke.id} joke={joke} />
             ))}
           </div>
         </Container>
