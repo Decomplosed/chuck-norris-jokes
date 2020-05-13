2252fcbab4b2b4138ff89601ae62f2d70badfdf5
Bart Frackiewicz <decomplosed@gmail.com>
2020-05-13 16:33:04 +0200

    implemented Badge as a label for Likes tab

--- src/App.js
+++ src/App.js
@@ -133,7 +133,7 @@ function App() {
           <AppBar position='sticky' style={{ marginBottom: 20 }}>
             <Tabs value={currentTab} onChange={changeTab} centered>
               <Tab label='Home' id='home-tab' aria-controls='home-panel' />
-              <Tab label='Likes' id='like-tab' aria-controls='like-panel' />
+              <Tab label={<Badge />} id='like-tab' aria-controls='like-panel' />
             </Tabs>
           </AppBar>
           <div role='tabpanel' hidden={currentTab !== 0}>
